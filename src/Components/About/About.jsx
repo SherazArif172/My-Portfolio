f5325945831cc32f66e8c5c41aceb10688ccfb9f
Sheraz Arif <sherazarifofficial@gmail.com>
2024-01-12 17:11:41 +0500

    update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -5,25 +5,25 @@ function About() {
   return (
     <div className="mt-52 grid lg:grid-cols-2">
       <div className="text-white m-auto lg:pl-20 pl-0 lg:mx-0 mx-20">
-        <h1 className="text-center text-4xl  mb-16">
+        <h1 className="text-center text-5xl  mb-16">
           Know Who <span className="text-purple-400">I'M</span>
         </h1>
         <p className="text-xl leading-9">
-          Hi Everyone, I am <span className="text-purple-500">Sheraz Arif</span>{" "}
-          from <span className="text-purple-500">Sargodha, Pakistan</span> . I
-          am currently employed as a software developer at Juspay. I have
-          completed Integrated MSc (IMSc) in Maths and Computing at BIT Mesra.
+          Hi Everyone, I am <span className="text-purple-400">Sheraz Arif</span>{" "}
+          from <span className="text-purple-400">Sargodha, Pakistan</span> . I
+          am currently employed as a Front-end Developer at IR Media. I have
+          completed my Front-end learning just before.
         </p>
         <p className="text-2xl leading-9 mt-7">
           Apart from coding, some other activities that I love to do!
         </p>
         <ul className="text-xl leading-9 mt-4 ml-8 ">
           <li className="flex content-center items-center">
-            <FaHandPointRight className="mr-2" /> Playing Games
+            <FaHandPointRight className="mr-2" /> Playing outdoor Games
           </li>
           <li className="flex content-center items-center">
             {" "}
-            <FaHandPointRight className="mr-2" /> Writing Tech Blogs
+            <FaHandPointRight className="mr-2" /> Watching Movies
           </li>
           <li className="flex content-center items-center">
             <FaHandPointRight className="mr-2" />
@@ -31,8 +31,8 @@ function About() {
           </li>
         </ul>
         <p className="text-purple-400 text-center text-xl leading-8 mt-6">
-          "Strive to build things that make a difference!" <br />
-          --Sheraz
+          "Think by Your own Mind!" <br />
+          -Sheraz
         </p>
       </div>
       <div className="m-auto lg:mt-0 mt-16">
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,32 +1,41 @@
 import React from "react";
-// import "./Navbar.css";
+import { FaBars } from "react-icons/fa";
+
 import {
   AiOutlineHome,
   AiOutlineProject,
   AiOutlineInfoCircle,
 } from "react-icons/ai";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <div className=" bg-black ">
-        <div className="flex justify-between mx-7  py-6">
+        <div className="flex justify-between items-center   ml-9  py-6">
           <h2 className="font-bold text-5xl text-purple-400">Sh.</h2>
-          <div>
-            <ul className="flex gap-7 text-white text-lg mr-9">
-              <li className="cursor-pointer  flex justify-center items-center gap-1 text-white hover:text-gray-300 transition duration-300 border-b-4 border-transparent hover:border-purple-400">
+
+          <div className="mt-8">
+            <ul className=" flex  gap-9  text-white text-lg mr-9  ">
+              <li className="cursor-pointer  flex lg:justify-center items-center gap-1 text-white hover:text-gray-300 transition duration-300 border-b-4 border-transparent hover:border-purple-400">
                 <AiOutlineHome />
                 <a href="/" className="styling">
                   Home
                 </a>
               </li>
-              <li className="cursor-pointer  flex justify-center items-center gap-1 hover:text-gray-300 transition duration-300 border-b-4 border-transparent hover:border-purple-400">
+              <li className="cursor-pointer  flex lg:justify-center items-center gap-1 text-white hover:text-gray-300 transition duration-300 border-b-4 border-transparent hover:border-purple-400">
                 <AiOutlineInfoCircle />
                 <a href="about" className="styling">
                   About
                 </a>
               </li>
-              <li className="cursor-pointer flex justify-center items-center gap-1 hover:text-gray-300 transition duration-300 border-b-4 border-transparent hover:border-purple-400">
+              <li className="cursor-pointer flex lg:justify-center items-center gap-1 text-white hover:text-gray-300 transition duration-300 border-b-4 border-transparent hover:border-purple-400">
                 <AiOutlineProject />
                 <a href="project" className="styling">
                   Projects
